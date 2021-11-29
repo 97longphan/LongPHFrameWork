69598d106e25b88ddd234e1691c6caec106b955b
ps1.longph <ps1.longph@techcombank.com.vn>
2021-11-29 15:59:59 +0700

    first init

---
+++ LongPHFrameWork/LongPHFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  LongPHFrameWork.h
+//  LongPHFrameWork
+//
+//  Created by ps1.longph on 29/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LongPHFrameWork.
+FOUNDATION_EXPORT double LongPHFrameWorkVersionNumber;
+
+//! Project version string for LongPHFrameWork.
+FOUNDATION_EXPORT const unsigned char LongPHFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LongPHFrameWork/PublicHeader.h>
+
+
---
+++ Source/LongPH.swift
@@ -0,0 +1,8 @@
+public class LongPh {
+    
+    static public let shared = LongPh()
+    
+    public func getName() {
+        print("get name function")
+    }
+}
